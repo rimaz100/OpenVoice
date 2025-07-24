ec5ba9a73f43fe6023ee68d347adfd065cd06379
rimaz100 <rimazmo18@gmail.com>
2025-07-24 14:42:56 +0300

    Update cleaners.py

--- openvoice/text/cleaners.py
+++ openvoice/text/cleaners.py
@@ -13,5 +13,7 @@ def cjke_cleaners2(text):
     text = re.sub(r'\[EN\](.*?)\[EN\]',
                   lambda x: english_to_ipa2(x.group(1))+' ', text)
     text = re.sub(r'\s+$', '', text)
+    text = re.sub(r'\[AR\](.*?)\[AR\]',
+                  lambda x: arabic_to_lazy_ipa(x.group(1)) + ' ', text)
     text = re.sub(r'([^\.,!\?\-…~])$', r'\1.', text)
     return text
