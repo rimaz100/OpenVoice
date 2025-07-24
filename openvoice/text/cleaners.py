8f2299f19372b0e8037bf05b4838bdf46974780d
rimaz100 <rimazmo18@gmail.com>
2025-07-24 14:41:03 +0300

    Update cleaners.py

--- openvoice/text/cleaners.py
+++ openvoice/text/cleaners.py
@@ -1,6 +1,7 @@
 import re
 from openvoice.text.english import english_to_lazy_ipa, english_to_ipa2, english_to_lazy_ipa2
 from openvoice.text.mandarin import number_to_chinese, chinese_to_bopomofo, latin_to_bopomofo, chinese_to_romaji, chinese_to_lazy_ipa, chinese_to_ipa, chinese_to_ipa2
+from openvoice.text.arabic import arabic_cleaners, arabic_to_lazy_ipa
 
 def cjke_cleaners2(text):
     text = re.sub(r'\[ZH\](.*?)\[ZH\]',
